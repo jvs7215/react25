b44c0958dcbbd80381aa69ce6e4fd61c06bac7a6
Jessica Salemme <jvs7215@psu.edu>
2025-11-11 01:59:54 -0500

    adding dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,48 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import batFlyingAnimationHighRes from './images/batFlyingAnimationHighRes.gif'
-import viteLogo from '/vite.svg'
+import githubMark from './images/githubMark.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
+    const [darkMode, setDarkMode] = useState(false)
 
-  return (
-    <>
+    useEffect(() => {
+        document.body.className = darkMode ? 'dark' : 'light'
+    }, [darkMode])
+
+    const toggleDarkMode = () => setDarkMode(!darkMode)
+
+    return (
         <div>
-            <a href="https://vite.dev" target="_blank" rel="noreferrer">
-                <img src={viteLogo} className="logo" alt="Vite logo" />
-            </a>
+            <div>
+                <a href="https://github.com/jvs7215" target="_blank" rel="noreferrer">
+                    <img src={githubMark} className="logo" alt="GitHub logo" />
+                </a>
 
-            <a href="#" rel="noreferrer">
-                <img
-                    src={batFlyingAnimationHighRes}
-                    className="logo bat"
-                    alt="Bat logo"
-                />
-            </a>
+                <a href="#">
+                    <img
+                        src={batFlyingAnimationHighRes}
+                        className="logo bat"
+                        alt="Bat logo"
+                    />
+                </a>
+            </div>
+
+            <h1>Let's test out some React</h1>
+
+            <div className="card">
+                <button onClick={() => setCount(count + 1)}>
+                    count is {count}
+                </button>
+                <button onClick={toggleDarkMode}>
+                    Toggle {darkMode ? 'Light' : 'Dark'} Mode
+                </button>
+            </div>
+
+            <p className="read-the-docs">Click on the GitHub logo to learn more</p>
         </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    )
 }
 
 export default App
